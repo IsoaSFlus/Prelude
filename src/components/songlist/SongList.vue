236b79bed0ee760d0874d6c88e568f3125159149
THMonster <me@isoasflus.com>
2022-10-04 11:27:12 +0800

    111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111

---
+++ src/components/songlist/SongList.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { Track } from "./interface";
+
+const props = defineProps({
+	tracks: Array<Track>,
+});
+</script>
+
+<template>
+	<div class="container">
+		<el-row type="flex" justify="center" class="song-list-header">
+			<el-col :span="1" class="col-left">
+				<span>#</span>
+			</el-col>
+			<el-col :span="22" class="col-left">
+				<span>标题</span>
+			</el-col>
+			<el-col :span="1" class="col-right">
+				<span>时长</span>
+			</el-col>
+		</el-row>
+		<template v-for="(t, i) in props.tracks" :key="t.id">
+			<el-row type="flex" justify="center" class="song-item">
+				<el-col :span="1" class="col-left">
+					<span class="index">{{ i + 1 }}</span>
+				</el-col>
+				<el-col :span="22" class="col-left">
+					<span>{{t.title}}</span>
+				</el-col>
+				<el-col :span="1" class="col-right">
+					<span class="duration">{{ t.duration }}</span>
+				</el-col>
+			</el-row>
+		</template>
+	</div>
+</template>
+
+<style lang="less" scoped>
+@global-width: 90%;
+
+.container {
+	margin-top: 2rem;
+	margin-bottom: 2rem;
+}
+
+.song-list-header {
+	width: @global-width;
+	margin: auto;
+}
+
+.song-item {
+	width: @global-width;
+	margin: auto;
+}
+
+.col-left {
+	text-align: left;
+}
+
+.col-right {
+	text-align: right;
+}
+
+.duration {
+	font-family: Courier, monospace;
+}
+
+.index {
+	.duration
+}
+</style>
--- src/views/AlbumDetailView.vue
+++ src/views/AlbumDetailView.vue
@@ -9,13 +9,8 @@ import { useRouter, useRoute } from "vue-router";
 import { invoke } from "@tauri-apps/api";
 import FindSameView from "./FindSameView.vue";
 import { Back } from "@element-plus/icons-vue";
-
-interface Track {
-	id: string;
-	duration: string;
-	title: string;
-	performers: string;
-}
+import { Track } from "../components/songlist/interface";
+import SongList from "../components/songlist/SongList.vue";
 
 const tracks: Ref<Array<Track>> = ref([]);
 const find_same_visible = ref(false);
@@ -83,8 +78,8 @@ onMounted(async () => {
 </script>
 
 <template>
-	<el-row class="adv-row" type="flex" justify="center">
-		<el-col :span="24">
+	<!-- <el-row class="adv-row" type="flex" justify="center"> -->
+		<!-- <el-col :span="24"> -->
 			<el-row class="adv-back-button-row" type="flex" justify="left">
 				<el-button class="adv-back-button" type="primary" @click="go_back" size="large"
 					:icon="Back" circle></el-button>
@@ -93,7 +88,7 @@ onMounted(async () => {
 				<img :src="route.params.cover_url as string" class="cover-image" />
 			</div>
 			<div class="adv-album-title">
-			<span >{{route.params.album_title}}</span>
+				<span>{{route.params.album_title}}</span>
 			</div>
 			<el-row class="adv-add-button-row" type="flex" justify="center">
 				<el-button class="adv-add-button" type="primary" @click="add_to_mpd" size="medium">Add
@@ -103,12 +98,9 @@ onMounted(async () => {
 				<el-button class="adv-add-button" type="primary" @click="find_same('qobuz')"
 					size="medium">Find in Qobuz</el-button>
 			</el-row>
-			<el-table :data="tracks">
-				<el-table-column prop="title" label="标题" width="600" />
-				<el-table-column prop="duration" label="时间" align="right" width="100" />
-			</el-table>
-		</el-col>
-	</el-row>
+			<song-list :tracks="tracks"></song-list>
+		<!-- </el-col> -->
+	<!-- </el-row> -->
 	<el-dialog v-model="find_same_visible" width="70%" title="Find the same album and add to mpd" destroy-on-close>
 		<find-same-view :site="find_same_site" :keywords="route.params.album_title as string"></find-same-view>
 	</el-dialog>
@@ -129,7 +121,7 @@ onMounted(async () => {
 	margin-right: 0.5rem;
 }
 
-.adv-album-title{
+.adv-album-title {
 	margin-top: 1.5rem;
 	margin-bottom: 1.5rem;
 	font-size: larger;
